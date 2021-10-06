6015a25a630736a1cd59bb81977bab22e21d5250
Lars Depuydt <lars.depuydt@gmail.com>
2021-10-06 11:32:32 +0200

    hardcoded frontend url

--- app.js
+++ app.js
@@ -29,7 +29,14 @@ const app = express();
 app.use(helmet());
 
 // enables cors
-const whitelist = process.env.FRONTEND_URL.split(" ");
+const whitelist2 = process.env.FRONTEND_URL.split(" ");
+console.log(whitelist2);
+const whitelist = [
+  "https://aanwezigheden.vercel.app/",
+  "https://aanwezigheden-larsdepuydt.vercel.app",
+  "https://aanwezigheden-7ucgu0q0w-larsdepuydt.vercel.app",
+  "https://aanwezigheden-git-master-larsdepuydt.vercel.app",
+];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
